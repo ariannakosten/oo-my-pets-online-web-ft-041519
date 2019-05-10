598e5b350f8ecc8fea70bf5b14992ff00a9833b1
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-10 22:44:18 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,13 +43,12 @@ class Owner
   end
   
   def sell_pets
-    pets.each do |species, animals|
-      animals.each do |animal|
-        animal.mood = "nervous"
-      end
-      animals.clear
-    end
-  end
+    # pets.each do |species, animals|
+    #   animals.each do |animal|
+    #     animal.mood = "nervous"
+    #   end
+    #   animals.clear
+    # end
   end 
   
   def list_pets
