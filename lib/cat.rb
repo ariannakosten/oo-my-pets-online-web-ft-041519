f7b2decd0e9f248937268e016589108736cef30f
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-10 22:02:04 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,10 @@
 class Cat
   
-  attr_accessor :mood
-  attr_reader :name
+  attr_accessor :mood #cat can change its mood
+  attr_reader :name   #cat cant change its name
   
-  def initialize(name)
+  def initialize(name) #initializes with a name
     @name = name
-    # @mood = "nervous"
+    # @mood = "nervous"  #initializes with a nervouse mood
   end
 end
