c47ee0bb1f2b81b6431c033d91182b337b09c3dc
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-10 22:03:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
- 
+  
+  attr_accessor :mood #dog can change its mood
+  attr_reader :name   #dog cant change its name
+  
+  def initialize(name) #initializes with a name
+    @name = name
+    @mood = "nervous"  #initializes with a nervouse mood
+  end
+end
 end
